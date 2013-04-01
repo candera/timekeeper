b1b117cde9daa42f7ed0465ef706a7a0905ef3c4
Craig Andera <candera@wangdera.com>
2013-03-31 22:58:56 -0400

    Add support for lein-launch4j

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject org.craigandera/timekeeper "0.1.0-SNAPSHOT"
+ (defproject org.craigandera/timekeeper "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
+  :launch4j-config-file "l4j.xml"
   :main timekeeper.main
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.2"]]
                    :source-paths ["dev"]}})
