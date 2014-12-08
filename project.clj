21706bd33a2015f005fe13183cc77b56584a4e89
Craig Andera <candera@wangdera.com>
2014-12-08 11:23:58 -0500

    Convert to use dynne.

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.craigandera/dynne "0.4.1"]]
   :launch4j-config-file "l4j.xml"
   :main timekeeper.main
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.2"]]
--- src/timekeeper/main.clj
+++ src/timekeeper/main.clj
@@ -1,59 +1,24 @@
 (ns timekeeper.main
+  (:require [dynne.sampled-sound :as dynne])
   (:gen-class)
   (:import [javax.sound.sampled AudioFormat AudioSystem Clip]))
-
-(defn sin-wav
-  "Returns a lazy seq of floats that are a pure sine wave of
-  frequency `freq` Hertz of duration `duration` seconds, assuming a
-  sample rate of `sample-rate`"
-  [freq duration sample-rate]
-  (for [i (range (* sample-rate duration))]
-    (Math/sin (/ (* i freq 2 Math/PI)
-                 sample-rate))))
-
-(defn envelope
-  "Returns a sequence of floating point numbers that climbs from 0 to 1
-  by `attack` seconds, stays at 1 for `sustain` seconds, goes to zero
-  after `decay` seconds, and is infinitely zero after that, assuming a
-  sample rate of `sample-rate`."
-  [attack sustain decay sample-rate]
-  (let [attack-samples (Math/round (* attack sample-rate))
-        sustain-samples (Math/round (* sustain sample-rate))
-        decay-samples (Math/round (* decay sample-rate))]
-    (concat
-     (map #(/ % (double attack-samples)) (range attack-samples))
-     (repeat sustain-samples 1.0)
-     (map #(- 1.0 (/ % (double decay-samples))) (range decay-samples))
-     (repeat 0.0))))
-
-(defn to-byte-array
-  "Returns `s` converted to a byte array. Assumes `s` is a seq of
-  floating points numbers on the range [-1.0 1.0]."
-  [s]
-  (byte-array (map #(byte (* Byte/MAX_VALUE %)) s)))
 
 (defn make-sin-clip
   "Returns a clip of sound that's a pure sine wave of frequency `freq`
   Hertz of duration `duration` seconds, with the envelope describe by
   `attack`, `sustain`, and `decay`."
   [freq duration attack sustain decay]
-  (let [sample-rate 16000
-        samples (map *
-                     (sin-wav freq duration sample-rate)
-                     (envelope attack sustain decay sample-rate))
-        buffer (to-byte-array samples)]
-    (doto (AudioSystem/getClip)
-      (.open (AudioFormat. sample-rate 8 1 true false)
-             buffer
-             0
-             (alength buffer)))))
+  (-> (dynne/sinusoid duration freq)
+      (dynne/envelope (dynne/segmented-linear 1
+                                              0.0 attack
+                                              1.0 sustain
+                                              1.0 decay
+                                              0.0))))
 
 (defn play
   "Plays a clip"
   [clip]
-  (doto clip
-    (.setFramePosition 0)
-    (.start)))
+  (dynne/play clip))
 
 (defn timer
   "Gives an audible cue for a set of `iterations` of on-off cycles.
